3c645bbad0af7de3b479273d596e9eac143552af
jopasNytOn <jopasnyton@saunalahti.fi>
2023-09-23 08:58:48 +0300

    Read filename from command line parameters

--- ProductsBestBeforeChecker.py
+++ ProductsBestBeforeChecker.py
@@ -2,6 +2,7 @@
 
 import datetime
 import re
+import sys
 
 def convert_product_date_to_real_date(product_date):
     match = re.search('(\d*)[\.\/\-](\d*)[\.\/\-](\d*)', product_date)
@@ -16,14 +17,15 @@ def convert_product_date_to_real_date(product_date):
     return False, product_date
 
 today = datetime.date.today()
-print("v0.2\n\nToday", today, "\n")
+print("v0.3\n\nToday", today, "\n")
 
-csv_lines = open("products.csv", 'r').read().split('\n')
-for line in csv_lines:
-    match = re.search('(.*); (.*); (.*)', line)
-    if match:
-        success, product_date = convert_product_date_to_real_date(match.group(3))
-        if success:
-            valid = product_date - today > datetime.timedelta(days=30)
-            if not valid:
-                print(match.group(1), product_date)
+if len(sys.argv) == 2:
+    csv_lines = open(sys.argv[1], 'r').read().split('\n')
+    for line in csv_lines:
+        match = re.search('(.*); (.*); (.*)', line)
+        if match:
+            success, product_date = convert_product_date_to_real_date(match.group(3))
+            if success:
+                valid = product_date - today > datetime.timedelta(days=30)
+                if not valid:
+                    print(match.group(1), product_date)
