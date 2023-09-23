d0670bc3c58635b561b73b2d4a8fa8352629e44c
jopasNytOn <jopasnyton@saunalahti.fi>
2023-09-23 13:40:36 +0300

    Create common setUp for unit tests

--- test_ProductsBestBeforeChecker.py
+++ test_ProductsBestBeforeChecker.py
@@ -7,65 +7,56 @@ def call_command(command):
 
 class TestProductsBestBeforeChecker(unittest.TestCase):
 
+    def setUp(self):
+        self.version = call_command("python ProductsBestBeforeChecker.py --version")
+
     def test_ValidDateWithDashes(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/valid_with_dashes.csv")
-        self.assertEqual(version, value)
+        self.assertEqual(self.version, value)
 
     def test_ValidDateWithPoints(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/valid_with_points.csv")
-        self.assertEqual(version, value)
+        self.assertEqual(self.version, value)
 
     def test_ValidDateWithSlashes(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/valid_with_slashes.csv")
-        self.assertEqual(version, value)
+        self.assertEqual(self.version, value)
 
     def test_ValidDateWithSpaces(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/valid_with_spaces.csv")
-        self.assertEqual(version, value)
+        self.assertEqual(self.version, value)
 
     def test_ValidDateWithoutDay(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/valid_without_day.csv")
-        self.assertEqual(version, value)
+        self.assertEqual(self.version, value)
 
     def test_ValidDateShortenedYear(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/valid_with_shortened_year.csv")
-        self.assertEqual(version, value)
+        self.assertEqual(self.version, value)
 
     def test_InvalidDateWithDashes(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/invalid_with_dashes.csv")
-        self.assertEqual(version + "\r\n\r\nabc 2022-11-22\r\ndef 2022-06-12", value)
+        self.assertEqual(self.version + "\r\n\r\nabc 2022-11-22\r\ndef 2022-06-12", value)
 
     def test_InvalidDateWithPoints(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/invalid_with_points.csv")
-        self.assertEqual(version + "\r\n\r\nabc 2022-11-22\r\ndef 2022-06-12", value)
+        self.assertEqual(self.version + "\r\n\r\nabc 2022-11-22\r\ndef 2022-06-12", value)
 
     def test_InvalidDateWithSlashes(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/invalid_with_slashes.csv")
-        self.assertEqual(version + "\r\n\r\nabc 2022-11-22\r\ndef 2022-06-12", value)
+        self.assertEqual(self.version + "\r\n\r\nabc 2022-11-22\r\ndef 2022-06-12", value)
 
     def test_InvalidDateWithSpaces(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/invalid_with_spaces.csv")
-        self.assertEqual(version + "\r\n\r\nabc 2022-11-22\r\ndef 2022-06-12", value)
+        self.assertEqual(self.version + "\r\n\r\nabc 2022-11-22\r\ndef 2022-06-12", value)
 
     def test_InvalidDateWithoutDay(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/invalid_without_day.csv")
-        self.assertEqual(version + "\r\n\r\nabc 2022-11-15\r\ndef 2022-06-15\r\nghi 2022-11-15\r\njkl 2022-06-15", value)
+        self.assertEqual(self.version + "\r\n\r\nabc 2022-11-15\r\ndef 2022-06-15\r\nghi 2022-11-15\r\njkl 2022-06-15", value)
 
     def test_InvalidDateShortenedYear(self):
-        version = call_command("python ProductsBestBeforeChecker.py --version")
         value = call_command("python ProductsBestBeforeChecker.py test/invalid_with_shortened_year.csv")
-        self.assertEqual(version + "\r\n\r\nabc 2022-11-22\r\ndef 2022-06-15", value)
+        self.assertEqual(self.version + "\r\n\r\nabc 2022-11-22\r\ndef 2022-06-15", value)
 
 if __name__ == '__main__':
     unittest.main()
